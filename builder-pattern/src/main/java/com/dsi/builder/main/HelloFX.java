af80a83e0332f6c4a539f22b2561a40bb0f8f881
Mich <michelcastillolmgp@gmail.com>
2020-11-09 20:33:52 -0300

    [DSI-01]: Adding first steps to process. Period.

--- builder-pattern/src/main/java/com/dsi/builder/main/HelloFX.java
+++ builder-pattern/src/main/java/com/dsi/builder/main/HelloFX.java
@@ -1,5 +1,10 @@
 package com.dsi.builder.main;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,7 +14,23 @@ import javafx.stage.Stage;
 public class HelloFX extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws ParseException {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.print("Ingrese fecha de Inicio: ");
+    	String fechaInicioStr = sc.nextLine();
+    	
+    	System.out.print("Ingrese fecha de Fin: ");
+    	String fechaFinStr = sc.nextLine();
+    	
+    	SimpleDateFormat sdf =  new SimpleDateFormat("dd/MM/yyyy");
+    	
+    	Date fechaInicio = sdf.parse(fechaInicioStr);
+    	Date fechaFin = sdf.parse(fechaFinStr);
+    	
+    	
+    	
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
