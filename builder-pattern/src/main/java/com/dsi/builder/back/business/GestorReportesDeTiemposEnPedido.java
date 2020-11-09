758ad7d3955c8e19a90644741312bf081d2be2e4
Mich <michelcastillolmgp@gmail.com>
2020-11-09 12:23:34 -0300

    [DSI-01]: Adding Gestor class

---
+++ builder-pattern/src/main/java/com/dsi/builder/back/business/GestorReportesDeTiemposEnPedido.java
@@ -0,0 +1,21 @@
+package com.dsi.builder.back.business;
+
+public class GestorReportesDeTiemposEnPedido {
+	
+	private String[] estadosSeleccionados;
+	private String fechaHoraActual;
+	private String nombreUsuarioLog;
+	private String sectoresSelecc;
+	private Double[][] tiemposPermMax;
+	private Double[][] tiemposPermMin;
+	private Double[][] tiemposPermDouble;
+	
+	public GestorReportesDeTiemposEnPedido() {};
+	
+	public void generarReportePDF() {};
+	
+	public void FormaVis() {};
+	
+	
+
+}
