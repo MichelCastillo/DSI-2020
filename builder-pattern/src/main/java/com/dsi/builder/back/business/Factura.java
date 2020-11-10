fcd12f5bada87f42e13878f6413683947aca2052
Mich <michelcastillolmgp@gmail.com>
2020-11-09 22:15:59 -0300

    [DSI-01]: Adding Factura - constructor

--- builder-pattern/src/main/java/com/dsi/builder/back/business/Factura.java
+++ builder-pattern/src/main/java/com/dsi/builder/back/business/Factura.java
@@ -1,6 +1,7 @@
 package com.dsi.builder.back.business;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -10,7 +11,19 @@ public class Factura {
 	private Date result;
 	private Date fecha;
 	private Date hora;
+	private int numero;
 	
+	public Factura(String fecha, int numeroFactura) throws ParseException {
+		
+		this.result = df.parse(fecha); // "10/10/2020T20:00:00+0000"
+
+		SimpleDateFormat sdf =  new SimpleDateFormat("dd/MM/yyyy");
+		this.fecha = sdf.parse(fecha);
+		
+		sdf =  new SimpleDateFormat("HH:mm");
+		this.hora = sdf.parse(fecha);
+		
+	}
 	
 
 }
