ccd66149bffd5fb1cdd6ca06494550de147de66a
Mich <michelcastillolmgp@gmail.com>
2020-11-09 22:05:52 -0300

    [DSI-01]: Adding Factura

--- builder-pattern/src/main/java/com/dsi/builder/back/business/Factura.java
+++ builder-pattern/src/main/java/com/dsi/builder/back/business/Factura.java
@@ -1,5 +1,16 @@
 package com.dsi.builder.back.business;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Factura {
+	
+	private DateFormat df = new SimpleDateFormat("dd/MM/yyyy'T'HH:mm:ssZ");
+	private Date result;
+	private Date fecha;
+	private Date hora;
+	
+	
 
 }
